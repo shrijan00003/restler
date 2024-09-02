7f8ea3f425b8a229b9b04061663eb340c5a985d6
Shrijan <shrijan00003@gmail.com>
2024-09-02 14:14:26 -0400

    feat/release-readme-update: release added to the readme

--- Readme.md
+++ Readme.md
@@ -4,16 +4,36 @@ Restler is a simple command line tool to make REST API calls.
 
 ## Introduction
 
-## Inspiration
-
 ## Installation
 
-go build -o restler bin/main.go
+### MacOS x86_64
+
+```bash
+curl -s https://raw.githubusercontent.com/shrijan00003/restler/main/install/darwin-amd64.sh | bash
+```
+
+### MacOS arm64
+
+```bash
+curl -s https://raw.githubusercontent.com/shrijan00003/restler/main/install/darwin-arm64.sh | bash
+```
+
+### Linux x86_64
+
+```bash
+curl -s https://raw.githubusercontent.com/shrijan00003/restler/main/install/linux-amd64.sh | bash
+```
 
 ## Usage
 
 ```
 export RESTLER_PATH=./example/restler
-Usage: ./rsclient posts
+restler posts
 
 ```
+
+## Inspiration
+
+## Build
+
+go build -o restler bin/main.go
---
+++ install/linux-amd64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Variables
+REPO="shrijan00003/restler"
+VERSION="v0.0.1-dev.1"
+BINARY_NAME="restler"
+TAR_FILE="${BINARY_NAME}-${VERSION}-linux-amd64.tar.gz"
+DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${VERSION}/${TAR_FILE}"
+
+# Download the tar.gz file
+echo "Downloading ${TAR_FILE} from ${DOWNLOAD_URL}..."
+curl -L -o /tmp/${TAR_FILE} ${DOWNLOAD_URL}
+
+# Extract the tar.gz file
+echo "Extracting ${TAR_FILE}..."
+tar -xzf /tmp/${TAR_FILE} -C /tmp
+
+# Move the binary to /usr/local/bin
+echo "Installing ${BINARY_NAME}..."
+sudo mv /tmp/${BINARY_NAME} /usr/local/bin/${BINARY_NAME}
+
+# Clean up
+echo "Cleaning up..."
+rm /tmp/${TAR_FILE}
+rm /tmp/${BINARY_NAME}
+
+echo "Installation completed. You can now run '${BINARY_NAME}' from anywhere."
